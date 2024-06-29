7fb6f884cb12a6024e4d5af8f48040275c14c012
ProstoVladi <155735377+ProstoVladi@users.noreply.github.com>
2024-06-29 09:05:42 +0000

    Deploying to gh-pages from @ VSanotskyi/codeMasters@1a1db6ede4964383f5948a11632ecb33b7127846 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,39 +1,39 @@
-import{a as N,t as _}from"./assets/vendor-f9df95ff.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))n(o);new MutationObserver(o=>{for(const a of o)if(a.type==="childList")for(const l of a.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&n(l)}).observe(document,{childList:!0,subtree:!0});function s(o){const a={};return o.integrity&&(a.integrity=o.integrity),o.referrerPolicy&&(a.referrerPolicy=o.referrerPolicy),o.crossOrigin==="use-credentials"?a.credentials="include":o.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function n(o){if(o.ep)return;o.ep=!0;const a=s(o);fetch(o.href,a)}})();const I="https://api.themoviedb.org/3",C="82b205270d637f8e7c805273646956b7",w="https://image.tmdb.org/t/p/w200/",O="/discover/movie",A="/genre/movie/list",B="/search/movie",T="/movie",G={api_key:C},p=N.create({baseURL:I,params:G}),U=async(e=1)=>{const{data:t}=await p(`${O}?page=${e}`);return t},E=async()=>{const{data:e}=await p(`${A}`);return e},j=async e=>{const{data:t}=await p(`${B}?query=${e}`);return t},z=async e=>{const{data:t}=await p(`${T}/${e}`);return t},f="page",y="total_pages",D=document.querySelector(".movies-list"),S=(e,t)=>{D.innerHTML=e.map(({title:s,poster_path:n,genre_ids:o,release_date:a,id:l})=>{const g=t.filter(r=>o.includes(r.id)).map(r=>r.name).join(", "),v=a.split("-")[0];return`
-    <li class="movies-item" data-id="${l}">
-      <img class="movies-img" src="${w+n}" alt=""/>
-      <p class="movies-title overflowWrap">${s}</p>
-      <p class="movies-text overflowWrap">${g} | ${v}</p>
-    </li>
-    `}).join("")},b=async e=>{try{const{page:t,total_pages:s,results:n}=await U(e),{genres:o}=await E();n.length>0&&S(n,o),s>1&&(localStorage.setItem(f,t),localStorage.setItem(y,s))}catch(t){console.log(t.message)}},Y=document.querySelector(".js__header-input");Y.addEventListener("input",_(K,2e3));async function K(e){const t=e.target.value;try{const{results:s}=await j(t),{genres:n}=await E();if(!s.length){b();return}S(s,n)}catch(s){console.log(s.message)}}b();const V=document.querySelector(".pagination-container"),W=document.querySelectorAll(".page"),F=document.querySelector(".prev"),H=document.querySelector(".next");let i=Number(localStorage.getItem(f))?Number(localStorage.getItem(f)):1;const c=Number(localStorage.getItem(y))>500?500:Number(localStorage.getItem(y)),d=5,M=()=>{let e=Math.floor(d/2),t=Math.max(i-e,1),s=Math.min(i+e,c);s-t+1<d&&(t===1?s=Math.min(t+d-1,c):s===c&&(t=Math.max(s-d+1,1))),W.forEach((n,o)=>{const a=t+o;n.textContent=String(a),a===i?n.classList.add("pagination-current"):n.classList.remove("pagination-current")}),F.disabled=i===1,H.disabled=i===c},R=async e=>{e.target.classList.contains("page")&&(i=Number(e.target.textContent)),e.target.classList.contains("prev")&&(i-=1),e.target.classList.contains("next")&&(i+=1),b(i),M()};M();V.addEventListener("click",R);const J=document.querySelector(".modal-wrapper"),Q=e=>{const{poster_path:t,original_title:s,genres:n,vote_average:o,vote_count:a,popularity:l,overview:g}=e,v=n.map(({name:k})=>k).join(" "),r=w+t,L=s.toUpperCase(),q=Math.round(l);J.innerHTML=`
-    <img class="modal-img" src="${r}" alt="">
+import{a as q,t as A}from"./assets/vendor-0d65446b.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const n of document.querySelectorAll('link[rel="modulepreload"]'))a(n);new MutationObserver(n=>{for(const s of n)if(s.type==="childList")for(const i of s.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function t(n){const s={};return n.integrity&&(s.integrity=n.integrity),n.referrerPolicy&&(s.referrerPolicy=n.referrerPolicy),n.crossOrigin==="use-credentials"?s.credentials="include":n.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function a(n){if(n.ep)return;n.ep=!0;const s=t(n);fetch(n.href,s)}})();const K="https://api.themoviedb.org/3",O="82b205270d637f8e7c805273646956b7",G="https://image.tmdb.org/t/p/w200/",N="/discover/movie",T="/genre/movie/list",U="/search/movie",D="/movie",m={baseURL:K,api_key:O,moviesEndPoint:N,defaultUrlImage:G,genreEndPoint:T,searchEndPoint:U,movieDetailsEndPoint:D},R={api_key:m.api_key},E=q.create({baseURL:m.baseURL,params:R}),j=async(e=1)=>{const{data:o}=await E(`${m.moviesEndPoint}?page=${e}`);return o},H=async()=>{const{data:e}=await E(`${m.genreEndPoint}`);return e},Y=async(e,o)=>{const{data:t}=await E(`${m.searchEndPoint}?page=${e}&query=${o}`);return t},z=async e=>{const{data:o}=await E(`${m.movieDetailsEndPoint}/${e}`);return o},L={getAllMovies:j,getGenres:H,getMovieByKeyword:Y,getMovieDetails:z},k=document.querySelector(".loader-backdrop"),V=async e=>{k.classList.toggle("is-hidden");try{return await L.getAllMovies(e)}catch(o){console.log(o.message)}finally{k.classList.toggle("is-hidden")}},x={getAllMovies:V},P=document.querySelector(".loader-backdrop"),W=async(e,o)=>{P.classList.toggle("loader-backdrop");try{return await L.getMovieByKeyword(e,o)}catch(t){console.log(t.message)}finally{P.classList.toggle("loader-backdrop")}},J={getMovieByKeyword:W},F="page",B={GENRES_KEY:F},Q="/codeMasters/assets/default-image-112f438b.jpg",X=document.querySelector(".movies-list"),Z=e=>{const o=JSON.parse(localStorage.getItem(B.GENRES_KEY));X.innerHTML=e.map(({title:t,poster_path:a,genre_ids:n,release_date:s,id:i})=>{const c=a===null?Q:m.defaultUrlImage+a,l=s.split("-")[0],d=o.filter(r=>n.includes(r.id)).map(r=>r.name).join(", ");return`
+        <li class="movies-item" data-id="${i}">
+          <img class="movies-img" src="${c}" alt="${t}"/>
+          <p class="movies-title overflowWrap">${t}</p>
+          <p class="movies-text overflowWrap">${d} | ${l}</p>
+        </li>
+      `}).join("")},u=5,w=(e,o="")=>{let t=1,a=1;const n=document.getElementById("pagination-container"),s=c=>{n.innerHTML="";const l=document.createElement("ul");l.classList.add("pagination-list");const d=document.createElement("li"),r=document.createElement("button");r.innerHTML="&#8592",r.classList.add("pagination-btn"),r.disabled=t===1,r.addEventListener("click",()=>{t>1&&i(t-1)}),d.appendChild(r),l.appendChild(d);let f=Math.max(1,t-Math.floor(u/2)),y=Math.min(c,t+Math.floor(u/2));t-1<Math.floor(u/2)&&(y=Math.min(a,u)),a-t<Math.floor(u/2)&&(f=Math.max(1,a-u+1));for(let p=f;p<=y;p+=1){const S=document.createElement("li"),v=document.createElement("button");v.classList.add("pagination-btn"),v.innerText=p.toString(),v.classList.toggle("pagination-current",p===t),v.addEventListener("click",()=>{t!==p&&i(p)}),S.appendChild(v),l.appendChild(S)}const $=document.createElement("li"),g=document.createElement("button");g.innerHTML="&#8594",g.classList.add("pagination-btn"),g.disabled=t===c,g.addEventListener("click",()=>{t<c&&i(t+1)}),$.appendChild(g),l.appendChild($),n.appendChild(l)},i=async c=>{t=c;const{total_pages:l,results:d}=await e(c,o);d&&(Z(d),a=l>500?500:l,s(a))};i(1)},ee=document.querySelector(".js__header-input"),te=e=>{const o=e.target.value.trim();o.length===0?w(x.getAllMovies):w(J.getMovieByKeyword,o)};ee.addEventListener("input",A(te,2e3));const oe=async()=>{try{const{genres:e}=await L.getGenres();localStorage.setItem(B.GENRES_KEY,JSON.stringify(e))}catch(e){console.log(e.message)}};oe();w(x.getAllMovies);const ne=document.querySelector(".modal-wrapper"),se=e=>{const{poster_path:o,original_title:t,genres:a,vote_average:n,vote_count:s,popularity:i,overview:c}=e,l=a.map(({name:y})=>y).join(" "),d=m.defaultUrlImage+o,r=t.toUpperCase(),f=Math.round(i);ne.innerHTML=`
+    <img class="modal-img" src="${d}" alt="">
     <div class="modal-text-content-wrapper">
-      <h3 class="modal-title">${L}</h3>
+      <h3 class="modal-title">${r}</h3>
       <ul class="modal-info-list">
         <li class="modal-info-item">
           <p class="modal-info-text">Vote / Votes</p>
-          <p class="modal-info-text">${o} / ${a}</p>
+          <p class="modal-info-text">${n} / ${s}</p>
         </li>
         <li class="modal-info-item">
           <p class="modal-info-text">Popularity</p>
-          <p class="modal-info-text">${q}</p>
+          <p class="modal-info-text">${f}</p>
         </li>
         <li class="modal-info-item">
           <p class="modal-info-text">Original Title</p>
-          <p class="modal-info-text">${L}</p>
+          <p class="modal-info-text">${r}</p>
         </li>
         <li class="modal-info-item">
           <p class="modal-info-text">Genre</p>
-          <p class="modal-info-text">${v}</p>
+          <p class="modal-info-text">${l}</p>
         </li>
       </ul>
       <p class="modal-about">
         <span class="modal-about-title">About</span>
-        ${g}
+        ${c}
       </p>
       <div class="modal-btn-wrapper">
         <button type="button" class="modal-btn">Add to watched</button>
         <button type="button" class="modal-btn">Add to queue</button>
       </div>
     </div>
-  `},X=document.querySelector(".movies-list"),m=document.querySelector(".backdrop"),x=document.querySelector(".modal-btn-close"),Z=async e=>{if(e.target.classList.contains("movies-list")===!0)return;const t=e.target.closest("li").dataset.id;try{const s=await z(t);m.classList.remove("is-hidden"),Q(s),window.addEventListener("keydown",$),x.addEventListener("click",u),m.addEventListener("click",P)}catch(s){console.log(s.message)}},u=()=>{m.classList.add("is-hidden"),window.removeEventListener("keydown",$),x.removeEventListener("click",u),m.removeEventListener("click",P)},$=e=>{e.code==="Escape"&&u()},P=e=>{e.target.classList.contains("backdrop")&&u()};X.addEventListener("click",Z);const h=document.querySelector(".btn-scroll-up");window.addEventListener("scroll",()=>{window.scrollY>250?h.style.display="flex":h.style.display="none"});h.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"})});
+  `},ae=document.querySelector(".movies-list"),h=document.querySelector(".backdrop"),C=document.querySelector(".modal-btn-close"),ie=async e=>{if(e.target.classList.contains("movies-list")===!0)return;const o=e.target.closest("li").dataset.id;try{const t=await L.getMovieDetails(o);h.classList.remove("is-hidden"),se(t),window.addEventListener("keydown",I),C.addEventListener("click",b),h.addEventListener("click",_)}catch(t){console.log(t.message)}},b=()=>{h.classList.add("is-hidden"),window.removeEventListener("keydown",I),C.removeEventListener("click",b),h.removeEventListener("click",_)},I=e=>{e.code==="Escape"&&b()},_=e=>{e.target.classList.contains("backdrop")&&b()};ae.addEventListener("click",ie);const M=document.querySelector(".btn-scroll-up");window.addEventListener("scroll",()=>{window.scrollY>250?M.style.display="flex":M.style.display="none"});M.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"})});
 //# sourceMappingURL=commonHelpers.js.map
