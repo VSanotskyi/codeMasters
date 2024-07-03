828af9508ec3c126ca0a24d9cd414bc6b0ea62ef
Vsanotskyi <vsanotskyi@gmail.com>
2024-07-03 13:16:27 +0300

    update svg, add event listener for btn watched, btn queue

--- src/js/constants/urls.js
+++ src/js/constants/urls.js
@@ -1,6 +1,6 @@
 const baseURL = 'https://api.themoviedb.org/3';
 const api_key = '82b205270d637f8e7c805273646956b7';
-const defaultUrlImage = 'https://image.tmdb.org/t/p/w200/';
+const defaultUrlImage = 'https://image.tmdb.org/t/p/w500/';
 const moviesEndPoint = '/discover/movie';
 const genreEndPoint = '/genre/movie/list';
 const searchEndPoint = '/search/movie';
